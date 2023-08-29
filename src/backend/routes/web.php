770d32a81501efdac178d50581f2d1c39116417a
mahdi <MahdiBagheriVar@gmail.com>
2023-08-29 13:11:24 +0330

    chore: start fronted react docker file & change nginx config

--- src/backend/routes/web.php
+++ src/backend/routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/crm/', function () {
     return view('welcome');
 });
 
-Route::get('/test', [\App\Http\Controllers\TestController::class,'index']);
+Route::get('/crm/test', [\App\Http\Controllers\TestController::class,'index']);
